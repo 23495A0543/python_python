212e05dd737a7e813304774d770ae735b0ce5ac5
23495A0543 <namesrinu18@gmail.com>
2024-11-12 23:31:00 +0530

    pratice

--- Task/task11.py
+++ Task/task11.py
@@ -4,3 +4,12 @@ Even numbers which are divided by 4 and 8 from 1 to 100
 filter the negative numbers using filter
 take float inputs separated by '-'
 """
+
+
+arr =[]
+def even():
+    for i in range(1,100):
+      if i % 2:
+          print(arr)
+          
+print(even())
---
+++ recursion.py
@@ -0,0 +1,9 @@
+def sequence(n):
+    if n < 0:
+        return n
+    print(sequence(n-1))
+sequence(7)
+
+
+
+
